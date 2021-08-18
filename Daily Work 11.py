cd3c5587f7f85f9b57eb660716560ece6b0c62b2
hillary.smith <hillary.smith@keyin.com>
2021-08-18 12:04:26 -0230

    matplotlib example

---
+++ Daily Work 11.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import math
+x = [1, 2, 3, 4, 5]
+y = [math.sin(1), math.sin(2), math.sin(3), math.sin(4), math.sin(5)]
+
+plt.plot(x, y)
+plt.title("Sin from 1-5")
+plt.show()
+
